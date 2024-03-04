279f636887b040b63473f5c17fc8858b688c9265
capital.phyoethurakyaw@gmail.com <capital.phyoethurakyaw@gmail.com>
2024-03-05 02:06:11 +0630

    PTK

---
+++ Portfolio/index.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Portfolio
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void SendMessage(object sender, EventArgs e)
+        {
+            Response.Write("1222");
+        }
+    }
+}
